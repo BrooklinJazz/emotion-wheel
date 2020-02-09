c2d3c2f4224d47bb93f32a97597d081eba671baf
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-02-08 17:24:38 -0700

    configure cypress viewport

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,6 +1,6 @@
 describe("Example Test", function() {
   it("With a Basic Expect", function() {
-    cy.visit("http://192.168.169.210:19006/");
+    cy.visit("");
     cy.get("[data-test-id='work']").contains('emotion wheel', {matchCase: false});
   });
 });
