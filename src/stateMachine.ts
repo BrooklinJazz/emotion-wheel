da30ed5c11a9062ced424e8cd1fa0482fe4df356
kealyd <kealy@freshprep.ca>
2020-02-18 20:42:56 -0800

    extract anger actions

--- src/stateMachine.ts
+++ src/stateMachine.ts
@@ -1,15 +1,26 @@
 import { Machine, interpret } from "xstate";
+import { allAngerActions } from "./angerActions";
 // TODO add fully fleshed states.
 // this fill is currently and example of:
 //  Bad -> Busy/Bored -> Indifferent/Apathetic or Pressured/Rushed
 // see example: https://xstate.js.org/viz/?gist=14bee9d8de6c10a5c48e0c9e1d3ec461 try to keep visualization up to date
-const objectFromArray = (arr = []) =>
-  arr.reduce((total, each) => {
-    return {
+
+export const objectFromArray = (arr = []) =>
+arr.reduce((total, each) => {
+  return {
+    ...total,
+    [each]: each
+  };
+}, {});
+
+export const objFromAction = arr =>
+  arr.reduce(
+    (total, each) => ({
       ...total,
-      [each]: each
-    };
-  }, {});
+      [each[0]]: { on: each[1] }
+    }),
+    {}
+  );
 
 const initialActions = objectFromArray([
   "anger",
@@ -19,100 +30,6 @@ const initialActions = objectFromArray([
   "sad",
   "surprise"
 ]);
-
-const angerActions = objectFromArray([
-  "let_down",
-  "humiliated",
-  "bitter",
-  "mad",
-  "aggressive",
-  "frustrated",
-  "distant",
-  "critical"
-]);
-
-const letDownActions = objectFromArray(["betrayed", "resentful"]);
-
-const humiliatedActions = {
-  disrespected: "disrespected",
-  ridiculed: "ridiculed"
-};
-
-const bitterActions = {
-  indignant: "indignant",
-  violated: "violated"
-};
-
-const madActions = {
-  furious: "furious",
-  jealous: "jealous"
-};
-
-const aggressiveActions = {
-  provoked: "provoked",
-  hostile: "hostile"
-};
-
-const frustratedActions = {
-  infuriated: "infuriated",
-  annoyed: "annoyed"
-};
-
-const distantActions = {
-  withdrawn: "withdrawn",
-  numb: "numb"
-};
-
-const criticalActions = {
-  skeptical: "skeptical",
-  dismissive: "dismissive"
-};
-
-const emptyObjFromArr = arr =>
-  arr.reduce(
-    (total, each) => ({
-      ...total,
-      [each]: {}
-    }),
-    {}
-  );
-
-const actionObjFromKeyAction = arr =>
-  arr.reduce(
-    (total, each) => ({
-      ...total,
-      [each[0]]: { on: each[1] }
-    }),
-    {}
-  );
-
-const allAngerActions = actionObjFromKeyAction([
-  ["anger", angerActions],
-  ["let_down", letDownActions],
-  ["humiliated", humiliatedActions],
-  ["bitter", bitterActions],
-  ["mad", madActions],
-  ["aggressive", aggressiveActions],
-  ["frustrated", frustratedActions],
-  ["distant", distantActions],
-  ["critical", criticalActions],
-  ["betrayed"],
-  ["resentful"],
-  ["disrespected"],
-  ["ridiculed"],
-  ["indignant"],
-  ["violated"],
-  ["furious"],
-  ["jealous"],
-  ["provoked"],
-  ["hostile"],
-  ["infuriated"],
-  ["annoyed"],
-  ["withdrawn"],
-  ["numb"],
-  ["skeptical"],
-  ["dismissive"]
-])
 
 const emotionStateMachine = Machine({
   id: "Emotion Wheel",
