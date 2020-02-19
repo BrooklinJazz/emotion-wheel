43a445fa7b8227ed49b2ce86322b580f90ae45ff
kealyd <kealy@freshprep.ca>
2020-02-18 19:49:18 -0800

    draft initial state machine

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,21 @@ import styled from "styled-components/native";
 import { emotionStateMachine, initialActions } from "./stateMachine";
 import { useMachine } from "@xstate/react";
 
-const Container = styled.View`
+const Background = '#F1ECCF';
+
+const Container = styled.View(({color}) => `
   flex: 1;
   align-items: center;
   justify-content: center;
-`;
+  background-color: ${Background};
+`);
 
 export default function App() {
   const [current, send] = useMachine(emotionStateMachine);
   const [state, setState] = useState("");
   return (
-    <>
-      <TextInput style={{ backgroundColor: "red" }} onChangeText={setState} />
-      <TouchableOpacity onPress={() => send(state)}>
-        Send State
-      </TouchableOpacity>
+    <Container>
       <Text data-test-id="work">Emotion Wheel</Text>
-      <Text data-test-id="work" onPress={() => console.log(current.value)}>state: {current.value.toString()}</Text>
-    </>
+    </Container>
   );
 }
--- src/stateMachine.ts
+++ src/stateMachine.ts
@@ -3,14 +3,14 @@ import { Machine, interpret } from "xstate";
 // this fill is currently and example of:
 //  Bad -> Busy/Bored -> Indifferent/Apathetic or Pressured/Rushed
 // see example: https://xstate.js.org/viz/?gist=14bee9d8de6c10a5c48e0c9e1d3ec461 try to keep visualization up to date
+
 export const initialActions = {
-  bad: "bad",
-  fearful: "fearful",
-  angry: "angry",
-  disgusted: "disgusted",
+  anger: "anger",
+  fear: "fear",
+  joy: "joy",
+  disgust: "disgust",
   sad: "sad",
-  happy: "happy",
-  surprised: "surprised"
+  surprise: "surprise"
 };
 
 const boredStateMachine = {
@@ -21,36 +21,17 @@ const boredStateMachine = {
     apathetic: {}
   }
 };
-const busyStateMachine = {
-  initial: "init",
-  states: {
-    init: { on: { pressured: "pressured", rushed: "rushed" } },
-    pressured: {},
-    rushed: {}
-  }
-};
-
-const badStateMachine = {
-  on: { cancel: "init" },
-  initial: "init",
-  states: {
-    init: { on: { bored: "bored" } },
-    bored: boredStateMachine,
-    busy: busyStateMachine
-  }
-};
 
 export const emotionStateMachine = Machine({
   id: "Emotion Wheel",
   initial: "init",
   states: {
     init: { on: initialActions },
-    bad: badStateMachine,
-    fearful: {},
-    angry: {},
-    disgusted: {},
+    anger: {},
+    fear: {},
+    joy: {},
+    disgust: {},
     sad: {},
-    happy: {},
-    surprised: {}
+    surprise: {}
   }
 });
