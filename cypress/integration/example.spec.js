914cdaf8d0e3be7c9205111b5a5f7bb953a8cebf
kealyd <kealy@freshprep.ca>
2020-04-30 19:12:15 -0700

    add cypress

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,6 +1,35 @@
-describe("Example Test", function() {
-  it("With a Basic Expect", function() {
+const angerEmotions = [
+  "LetDown",
+  "Humiliated",
+  "Bitter",
+  "Mad",
+  "Aggressive",
+  "Frustrated",
+  "Distant",
+  "Critical"
+];
+
+const baseEmotions = ["Disgust", "Fear", "Joy", "Surprise", "Anger", "Sadness"];
+
+describe("Landing Page", function() {
+  it("has a start button", function() {
     cy.visit("");
-    cy.get("[data-test-id='work']").contains('emotion wheel', {matchCase: false});
+    cy.get("[data-test-id='anger']").should("not.exist");
+
+    cy.get("[data-test-id='startBtn']").click();
+    cy.get("[data-test-id='startBtn']").should("not.exist");
+    cy.get("[data-test-id='fear']");
+    cy.get("[data-test-id='disgust']");
+    cy.get("[data-test-id='joy']");
+    cy.get("[data-test-id='surprise']");
+    cy.get("[data-test-id='sadness']");
+
+    cy.get("[data-test-id='anger']").click();
+    angerEmotions.forEach(emotion => {
+      cy.get(`[data-test-id=${emotion.toLowerCase()}]`);
+    });
+    baseEmotions.forEach(emotion => {
+      cy.get(`[data-test-id=${emotion.toLowerCase()}]`).should("not.exist");
+    });
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,58 @@ import {
   Text,
   View,
   TouchableOpacity,
+  Button,
   TextInput
 } from "react-native";
 import { emotionStateMachine, initialActions } from "./xstate/stateMachine";
 import { useMachine } from "@xstate/react";
 import { Container } from "./Background";
+import { send } from "xstate";
+
+const baseEmotions = ["Disgust", "Fear", "Joy", "Surprise", "Anger", "Sadness"];
+const angerEmotions = [
+  "LetDown",
+  "Humiliated",
+  "Bitter",
+  "Mad",
+  "Aggressive",
+  "Frustrated",
+  "Distant",
+  "Critical"
+];
+
+const EmotionButton = ({ emotion, send }) => (
+  <View data-test-id={emotion.toLowerCase()}>
+    <Button title={emotion} onPress={() => send(emotion.toLowerCase())} />
+  </View>
+);
+
+const AngerEmotions = () => (
+  <View>
+    {angerEmotions.map(emotion => (
+      <EmotionButton emotion={emotion.toLowerCase()} />
+    ))}
+  </View>
+);
 
 export default function App() {
   const [current, send] = useMachine(emotionStateMachine);
+  const [isStarted, setIsStarted] = useState();
+
   return (
     <Container>
-      <Text data-test-id="work">Emotion Wheel</Text>
+      {!isStarted && (
+        <TouchableOpacity onPress={() => setIsStarted(true)}>
+          <Text data-test-id="startBtn">Start</Text>
+        </TouchableOpacity>
+      )}
+      <View>
+        {isStarted && current.value === 'init' &&
+          baseEmotions.map(emotion => (
+            <EmotionButton emotion={emotion} send={send} />
+          ))}
+      </View>
+      {current.value === "anger" && <AngerEmotions />}
     </Container>
   );
 }
